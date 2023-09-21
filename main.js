871dcafed798ece36a988dfcbf6cbdced44cec85
Anthony Obiako <anthonyobiakoc@gmail.com>
2023-09-21 13:04:04 +0100

    minor changes

--- main.js
+++ main.js
@@ -4,18 +4,28 @@ const computerChoice = document.getElementById(
 );
 const result = document.getElementById("result");
 const gameChoice = document.querySelectorAll("button");
+const scores = document.getElementById("scores");
 let userChoice;
 let computerPick;
 let gameResult;
-gameChoice.forEach((gamechoices) =>
-  gamechoices.addEventListener("click", (e) => {
-    userChoice = e.target.id;
-    myChoice.innerHTML = userChoice;
-    spawnComputerChoice();
-    gameWinner();
-  })
-);
+const userScores = document.getElementById("user-scores");
+const alienScores = document.getElementById("alien-scores");
+let userScore = 0;
+let alienScore = 0;
+const endText = document.getElementById("end-text");
+let endtexts;
 
+function initializeGame() {
+  gameChoice.forEach((gamechoices) =>
+    gamechoices.addEventListener("click", (e) => {
+      userChoice = e.target.id;
+      myChoice.innerHTML = userChoice;
+      spawnComputerChoice();
+      gameWinner();
+    })
+  );
+}
+initializeGame();
 function spawnComputerChoice() {
   const randomPick = Math.floor(
     Math.random() * gameChoice.length + 1
@@ -32,6 +42,15 @@ function spawnComputerChoice() {
   computerChoice.innerHTML = computerPick;
 }
 
+// function gameRound() {
+//   if (userScore === 3) {
+//     endtexts =
+//       "Brave Child you have saved this world . You are a true HERO Congratulations!!";
+//   } else if (alienScore === 3) {
+//     endtexts = "You Failed and now you are DOOMED";
+//   }
+// }
+
 function gameWinner() {
   if (userChoice === computerPick) {
     gameResult = " Its a draw";
@@ -41,12 +60,17 @@ function gameWinner() {
     (userChoice === "scissors" && computerPick === "paper")
   ) {
     gameResult = " You Win";
+    userScore++;
+    userScores.innerHTML = userScore;
   } else if (
     (userChoice === "rock" && computerPick === "paper") ||
     (userChoice === "paper" && computerPick === "scissors") ||
     (userChoice === "scissors" && computerPick === "rock")
   ) {
     gameResult = " You lose";
+    alienScore++;
+    alienScores.innerHTML = alienScore;
+    gameRound();
   }
 
   result.innerHTML = gameResult;
